78d8e5a981c18c3810a1dc97674f1649fc372808
halobartku <bartosz@easykotlet.com>
2024-11-14 19:55:00 +0100

    1

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -84,7 +84,7 @@ export function AboutUs() {
                   muted 
                   playsInline
                 >
-                  <source src="https://findprimarywater.com/images/PrimaryWater1.mp4" type="video/mp4" />
+                  <source src="https://wodapierwotna.pl/images/PrimaryWater1.mp4" type="video/mp4" />
                 </video>
               </div>
               <p className="text-blue-700 text-sm leading-relaxed">
